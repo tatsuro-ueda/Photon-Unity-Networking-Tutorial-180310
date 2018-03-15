9c7d9f2d9829302ef4ecbff3322fbbe45f166224
Tatsuro Ueda <tatsuro.ueda@feelphysics.education>
2018-03-15 18:11:14 +0900

    「パート3 - ゲームシーン＞設定のシーンリストを作成」まで： https://doc.photonengine.com/ja-jp/pun/current/demos-and-tutorials/pun-basics-tutorial/game-scenes#build_settings

---
+++ Prefabs/GameManager/GameManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Education.FeelPhysics.PhotonTutorial
+{
+    public class GameManager : MonoBehaviour
+    {
+
+        #region Photon Messages
+
+        /// <summary>
+        /// ローカルプレイヤーが退室するときに呼ばれる。Launcherシーンをロードしなければならない。
+        /// </summary>
+        public void OnLeftRoom()
+        {
+            SceneManager.LoadScene(0);
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public void LeaveRoom()
+        {
+            PhotonNetwork.LeaveRoom();
+        }
+
+        #endregion
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
--- readme.md
+++ readme.md
@@ -5,6 +5,25 @@
 - PUN（Photon Unity Network）  
 1.88
 
+### 使い方
+
+1. 新しくプロジェクトを作って下さい
+2. Asset Storeから**Photon Unity Network Free**をダウンロードし、インポートして下さい
+3. *Asset*フォルダの中に*Tutorial*フォルダを作って下さい
+3. 本レポジトリをダウンロードして下さい
+5. ダウンロードしたものを*Tutorial*フォルダに移動させて下さい
+2. **Build Settings**の**Scene List**に*Tutorial/Scenes*フォルダに入っているシーンを追加して下さい
+3. Gitクライアントに*Tutorial*フォルダを**Add**して下さい
+8. あとは自由にコミットを切り替えることができます
+
+### 補足
+
+#### Rect Transform Anchor Presets の変え方
+
+![Anchorの変え方](Others/Anchorの変え方.png)
+
+#### PUNのバージョン
+
 なお、PUNのバージョンは以下のようにして確かめることができます：
 
 ```csharp
