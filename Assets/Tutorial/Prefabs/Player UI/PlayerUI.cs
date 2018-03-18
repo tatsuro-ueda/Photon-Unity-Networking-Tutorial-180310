135feb7f05cb60cec4c71d857552b2e75f511cee
Tatsuro Ueda <tatsuro.ueda@feelphysics.education>
2018-03-19 06:22:43 +0900

    ＋「パート 9 - プレーヤーUIプレハブ＞インスタンス化とプレイヤーとの結合」まで： https://doc.photonengine.com/ja-jp/pun/current/demos-and-tutorials/pun-basics-tutorial/player-ui-prefab#bind

---
+++ Assets/Tutorial/Prefabs/Player UI/PlayerUI.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Education.FeelPhysics.PhotonTutorial
+{
+    public class PlayerUI : MonoBehaviour
+    {
+
+        #region Public Valuables
+
+        [Tooltip("プレイヤー名を表示する表示するUIテキスト")]
+        public Text PlayerNameText;
+
+        [Tooltip("プレイヤーの体力を表示するUIスライダ")]
+        public Slider PlayerHealthSlider;
+
+        #endregion
+
+        #region Private Valuables
+
+        // 定期的に体力をルックアップすることになるので、計画的に考える必要があります。
+        // 効率を良くするためにPlayerManagerの参照をキャッシュすると有効です。
+        PlayerManager _target;
+
+        #endregion
+
+        #region MonoBehaviour CallBacks
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            // プレイヤーの体力を反映させる
+            if (PlayerHealthSlider != null)
+            {
+                PlayerHealthSlider.value = _target.Health;
+            }
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public void SetTarget(PlayerManager target)
+        {
+            if (target == null)
+            {
+                Debug.LogError(MyHelper.FileAndMethodNameWithMessage("PlayerManager targetが<Color-Red>ありません</Color>"));
+            }
+            // 効率化のために参照をキャッシュする
+            _target = target;
+            if (PlayerNameText != null)
+            {
+                PlayerNameText.text = _target.photonView.owner.NickName;
+            }
+        }
+
+        #endregion
+    }
+}
